c30af6c7eb2a947b5493579b2ca51edc2937a2d9
Yauhenia_Zhdanovich <e.zhdanovich@hqsoftwarelab.com>
2021-12-21 10:50:20 +0400

    Add initial project structure, configs, basic component

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { App } from './components/App';
+
+ReactDom.render(
+    <App userName="Beveloper" lang="Typescript"/>,
+    document.getElementById('react-dom-container')
+);
